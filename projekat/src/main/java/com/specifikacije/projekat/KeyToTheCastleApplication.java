d59e005fb4eba8a33a61c7db7f7d59aead78236b
Miodrag Ugrica <ugrica.sr7.2022@uns.ac.rs>
2024-02-21 17:05:18 +0100

    Uradjen dijagram aktivnosti

---
+++ projekat/src/main/java/com/specifikacije/projekat/KeyToTheCastleApplication.java
@@ -0,0 +1,13 @@
+package com.specifikacije.projekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyToTheCastleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyToTheCastleApplication.class, args);
+	}
+
+}
---
+++ projekat/src/test/java/com/specifikacije/projekat/KeyToTheCastleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.specifikacije.projekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyToTheCastleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
