5ffb5af52d4090efcea898e635ab5b1c45f0826b
Miodrag Ugrica <ugrica.sr7.2022@uns.ac.rs>
2024-02-26 16:54:28 +0100

    Napravljena tabela za izvestaje

--- projekat/src/main/java/com/specifikacije/projekat/model/Report.java
+++ projekat/src/main/java/com/specifikacije/projekat/model/Report.java
@@ -1,5 +1,68 @@
 package com.specifikacije.projekat.model;
 
-public class Report {
+import java.time.LocalDateTime;
 
+public class Report {
+	private Long id;
+	private Administrator admin;
+	private LocalDateTime date;
+	private double income;
+	
+
+
+	public Report(Long id, Administrator admin, LocalDateTime date, double income) {
+		super();
+		this.id = id;
+		this.admin = admin;
+		this.date = date;
+		this.income = income;
+	}
+
+
+	public Report(Administrator admin, LocalDateTime date, double income) {
+		super();
+		this.admin = admin;
+		this.date = date;
+		this.income = income;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public Administrator getAdmin() {
+		return admin;
+	}
+
+
+	public void setAdmin(Administrator admin) {
+		this.admin = admin;
+	}
+
+
+	public double getIncome() {
+		return income;
+	}
+
+
+	public void setIncome(double income) {
+		this.income = income;
+	}
+
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
 }
